7206253cf21f280280c1060dc72435813ecb306d
Barnabas-ST <smarttraxto@gmail.com>
2022-10-02 21:42:26 -0400

    added Remove First and Last Character 8kyu Codewars

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 // String repeat JavaScript:
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
 
 function repeatStr (n, s) {
   
     return (n > 0 ? s.repeat(n) : 'not valid')
   
   }
+
+  // Remove First and Last Character
+  // Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+  function removeChar(str){
+    //You got this!
+     return str.slice(1, str.length-1)
+   };
+
+  // Multiply
+  // This code does not execute properly. Try to figure out why.
+
+    const multiply = (a, b) => a * b;
