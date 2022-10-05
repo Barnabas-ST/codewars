8bfcdcc1a35ba9408182a4bc0e43b803360ae161
Barnabas-ST <smarttraxto@gmail.com>
2022-10-04 22:56:24 -0400

    added Square (n) Sum codewars and alternative best practice solution

--- js/main.js
+++ js/main.js
@@ -37,5 +37,23 @@ function repeatStr (n, s) {
 
 
     function noSpace (x){
-      return x.split(' ').join('');
+      return x.split(' ').join(''); 
     }
+
+    // Square(n) Sum
+    /*  Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+      For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.*/
+
+      function squareSum(numbers){
+        const square = numbers.map( n => n*n);
+        const sum = square.reduce ( (acc, c) => acc + c, 0 );
+        return sum;
+      }
+
+      //  Alternative
+      //   function squareSum(numbers){
+      //   return numbers.reduce(function(sum, n){
+      //     return (n*n) + sum;
+      //   }, 0)
+      // }
