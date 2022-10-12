7899b0d94a4638dc2cf0c6d16a78bcadba9c38b2
Barnabas-ST <smarttraxto@gmail.com>
2022-10-12 12:37:51 -0400

    added my solution to Returning Strings codewars problem

--- js/main.js
+++ js/main.js
@@ -125,3 +125,18 @@ function repeatStr (n, s) {
         
         return centuryOfYear;
       }
+
+      // Returning Strings
+      // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+      function greet(name){
+        //your code here
+        
+        return `Hello, ${name} how are you doing today?`
+      }
+
+
+      //
+      //
+
+      
