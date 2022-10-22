00837cdcb5e76e985dd42276a1341039eec47100
Barnabas-ST <smarttraxto@gmail.com>
2022-10-21 23:58:44 -0400

    added Beginner Series #1 School Paperwork codewars solution

--- js/main.js
+++ js/main.js
@@ -334,6 +334,20 @@ function repeatStr (n, s) {
         return str.toUpperCase()
       }
 
+      // Beginner Series #1 School Paperwork
+      /* Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.  
+      Example 
+      n= 5, m=5: 
+      25 n=-5, m=5:  0
+      */ 
+
+      function paperwork (n, m) {
+  
+        return n < 0 || m < 0 ? 0 : m * n
+        
+      }
+      
+
       
 
 
