6dfea1957b708d5621c167c41efbffd554d70c53
Barnabas-ST <smarttraxto@gmail.com>
2022-10-27 08:10:21 -0400

    added my solution to the Are You Playing Banjo? codewars problem

--- js/main.js
+++ js/main.js
@@ -383,6 +383,18 @@ function repeatStr (n, s) {
       
       return invertedArray
     }
+
+    // Are You Playing Banjo?
+    /* Create a function which answers the question "Are you playing banjo?". If your name starts with the letter "R" or lower case "r", you are playing banjo!
+        The function takes a name as its only argument, and returns one of the following strings: 
+        name + " plays banjo"  and name + " does not play banjo"*/
+
+    function areYouPlayingBanjo(name) {
+      // Implement me
+      
+      return name.startsWith('R') || name.startsWith("r") ? name + " plays banjo" : name + " does not play banjo"
+    
+    }
       
 
       
