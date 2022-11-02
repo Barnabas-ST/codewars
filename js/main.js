0ecfd688bc4cdc2b10bcab519d7e2f6aac287147
Barnabas-ST <smarttraxto@gmail.com>
2022-11-01 22:15:44 -0400

    added my solution to Calculate BMI codewars problem

--- js/main.js
+++ js/main.js
@@ -476,3 +476,31 @@ function hero(bullets, dragons){
     return bullets >= dragons*2 ? true : false
     
   }
+
+
+  // Calculate BMI
+  /* Write function bmi that calculates body mass index (bmi = weight / height2). if bmi <= 18.5 return "Underweight"
+    if bmi <= 25.0 return "Normal"; if bmi <= 30.0 return "Overweight"; if bmi > 30 return "Obese" */
+
+  function bmi(weight, height) {
+  
+    const bmiVal = weight / Math.pow(height,2)
+    
+    if (bmiVal <= 18.5) {
+      
+      return "Underweight"
+      
+    } else if (bmiVal <= 25) {
+      
+     return "Normal"
+      
+    } else if (bmiVal <= 30){
+      
+      return "Overweight"
+      
+    } else if (bmiVal > 30){
+      
+      return "Obese"
+      
+    }
+  }
