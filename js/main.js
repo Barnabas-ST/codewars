19bd297edf8b79a6b7fd009062d9a85e469002b9
Barnabas-ST <smarttraxto@gmail.com>
2022-11-03 23:28:02 -0400

    added my solution to Reversed sequence codewars problem

--- js/main.js
+++ js/main.js
@@ -519,3 +519,18 @@ function hero(bullets, dragons){
     return rnaArr.join('')
     
   }
+
+
+  //  Reversed sequence
+  //  Build a function that returns an array of integers from n to 1 where n>0. Example : n=5 --> [5,4,3,2,1]
+
+  const reverseSeq = n => {
+  
+    let arr = []
+    
+    for (let i = 1; i <= n; i++){
+       arr.push(i)
+    }
+  
+      return arr.reverse()
+  };
