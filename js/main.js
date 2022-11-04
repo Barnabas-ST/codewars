d571079bc24a0e4d89e356828de3fd23d767e18f
Barnabas-ST <smarttraxto@gmail.com>
2022-11-04 18:25:25 -0400

    added my solution to Find Maximum and Minimum values of a List codewars problem

--- js/main.js
+++ js/main.js
@@ -534,3 +534,20 @@ function hero(bullets, dragons){
   
       return arr.reverse()
   };
+
+
+  //  Find Maximum and Minimum Values of a List
+  //  Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+  //  Examples (Input -> Output) * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+
+
+  var min = function(list){
+    
+    return Math.min(...list);
+
+}
+
+var max = function(list){
+    
+    return Math.max(...list);
+}
