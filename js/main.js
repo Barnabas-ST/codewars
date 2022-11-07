82003f347323c75412b37a53d900398536a8ede3
Barnabas-ST <smarttraxto@gmail.com>
2022-11-07 07:39:44 -0500

    added my solution to Beginner - Reduce but Grow codewars problem

--- js/main.js
+++ js/main.js
@@ -581,3 +581,11 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   
   return fuelLeft >= gallonsNeeded ? true : false
 };
+
+
+//  Beginner - Reduce but Grow
+//  Given a non-empty array of integers, return the result of multiplying the values together in order. Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  return x.reduce((a,c) => a * c, 1) 
+}
