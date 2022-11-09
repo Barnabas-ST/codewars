9f007bd6bc444026cd26426bbd8725af0dbe8333
Barnabas-ST <smarttraxto@gmail.com>
2022-11-09 08:22:59 -0500

    added my solution to String To an Array codewars problem

--- js/main.js
+++ js/main.js
@@ -598,3 +598,16 @@ function grow(x){
 function smash (words) {
   return words.join(' ')
 };
+
+
+//  Convert a string to an array
+//  Write a function to split a string and convert it into an array of words.
+//  Examples (Input ==> Output): "Robin Singh" ==> ["Robin", "Singh"] / "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+  return string.split(' ' );
+    // code code code
+  
+  }
+
+  
