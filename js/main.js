8062e94eb008406fdd20ddbc00a6f512b70ca891
Barnabas-ST <smarttraxto@gmail.com>
2022-11-15 08:11:40 -0500

    added my solution to Total amount of points codewars problem

--- js/main.js
+++ js/main.js
@@ -720,3 +720,42 @@ class Kata {
     return length*width*height
   }
 }
+
+
+//  Total amount of points
+/* Our football team has finished the championship. Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+For example: ["3:1", "2:2", "0:1", ...]. Points are awarded for each match as follows:
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+Notes: our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+  
+  let points = 0;
+  
+games.forEach(game => {
+    const results = game.split(":") 
+    const x = Number(results[0])
+    const y = Number(results[1])
+    
+    if (x>y){
+      points += 3;
+    } else if (x==y){
+      points += 1;
+    }
+  })
+
+  return points
+}
+
+//OR------
+
+function points(games) {
+  
+  return games.reduce ((w,[x,_,y]) => w + (x>y ? 3 : x==y), 0)
+}
