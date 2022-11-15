ac441d024e4304e3a7d03928c258e3435969ca6e
Barnabas-ST <smarttraxto@gmail.com>
2022-11-15 07:26:36 -0500

    added my solution to Volume of a Cuboid codewars problem

--- js/main.js
+++ js/main.js
@@ -708,3 +708,15 @@ const rps = (p1, p2) => {
   }
 
 }
+
+
+//  Volume of a Cuboid
+//  Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    
+    return length*width*height
+  }
+}
