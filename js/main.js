955b1cf3e445dd3907640eb3731971dbe6817bf6
Barnabas-ST <smarttraxto@gmail.com>
2022-11-16 06:30:01 -0500

    Added my solution to Area or Perimeter codewars problem

--- js/main.js
+++ js/main.js
@@ -759,3 +759,26 @@ function points(games) {
   
   return games.reduce ((w,[x,_,y]) => w + (x>y ? 3 : x==y), 0)
 }
+
+
+//  Area or Perimeter
+/*  You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9*/
+
+const areaOrPerimeter = function(l , w) {
+  // Return your answer
+  
+  if (l>w || l<w){
+    return l+l+w+w
+    
+  } else if (l===w){
+    return l*l
+    
+  } else {
+    return "not a shape" 
+  }
+};
