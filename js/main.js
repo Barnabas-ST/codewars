ac62bd954725a91425ae0d495c255293d270dfd0
Barnabas-ST <smarttraxto@gmail.com>
2022-11-17 07:03:34 -0500

    added my solution to Grasshopper - Grade book codewars problem

--- js/main.js
+++ js/main.js
@@ -782,3 +782,39 @@ const areaOrPerimeter = function(l , w) {
     return "not a shape" 
   }
 };
+
+
+//  Grasshopper - Grade book
+//  Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+/*
+Numerical Score	      Letter Grade
+90 <= score <= 100	      'A'
+80 <= score < 90	        'B'
+70 <= score < 80	        'C'
+60 <= score < 70	        'D'
+0 <= score < 60	          'F'
+*/
+
+
+function getGrade (s1, s2, s3) {
+  // Code here
+  
+  const avg = (s1+s2+s3)/3
+  
+   if(avg >= 90 && avg <= 100){
+     return 'A'
+     
+   }else if(avg >= 80 && avg < 90){
+     return 'B'
+     
+   }else if(avg >= 70 && avg < 80){
+     return 'C'
+     
+   }else if(avg >= 60 && avg < 70){
+     return 'D'
+     
+   }else if(avg >= 0 && avg <= 60 ){
+     return 'F'
+     
+   }
+}
