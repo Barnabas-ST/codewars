cb565f71a980716fccc4ae619795958f5e6cb452
Barnabas-ST <smarttraxto@gmail.com>
2022-11-19 08:41:37 -0500

    added my solution to Sum Mixed Array codewars problem

--- js/main.js
+++ js/main.js
@@ -838,3 +838,15 @@ const quarterOf = (month) => {
     return "Not in the calendar"
   }
 }
+
+
+//  Sum Mixed Array
+//  Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//  Return your answer as a number.
+
+function sumMix(x){
+
+  const xNum = x.map(e => parseInt(e))
+  
+  return xNum.reduce((a,c) => a+c,0)
+}
