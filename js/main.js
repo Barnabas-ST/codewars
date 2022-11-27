8fda27c0025e26c75fd580d2a9839670fb049a10
Barnabas-ST <smarttraxto@gmail.com>
2022-11-27 16:20:25 -0500

    added my solution to Beginner Series #4 Cockroach codewars problem

--- js/main.js
+++ js/main.js
@@ -1015,3 +1015,16 @@ function sumArray(array) {
         
         return charDoubled
       }
+
+
+      //  Beginner Series #4 Cockroach
+      /*  The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+      For example: 1.08 --> 30*/
+
+      function cockroachSpeed(s) {
+        //Good Luck!
+        
+        return Math.floor((s*30)/1.08)
+        
+      }
