06d23522677b4941b522a9e6ff5066e5256dcab8
Barnabas-ST <smarttraxto@gmail.com>
2022-12-03 10:34:33 -0500

    added my solution to Will there be enough space codewars problem

--- js/main.js
+++ js/main.js
@@ -1070,12 +1070,12 @@ function sumArray(array) {
           }
 
 
-          //  Twice as old
-          /*Your function takes two arguments:
-            current father's age (years)
-            current  age of his son (years)
-            Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
-          */
+        //  Twice as old
+        /*Your function takes two arguments:
+        current father's age (years)
+        current  age of his son (years)
+        Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+        */
 
           function twiceAsOld(dadYearsOld, sonYearsOld) {
             // your code here
@@ -1084,9 +1084,9 @@ function sumArray(array) {
           }
 
 
-          //  Switch it Up!
-          /*When provided with a number between 0-9, return it in words. Input :: 1 - Output :: "One".
-          If your language supports it, try using a switch statement.*/
+        //  Switch it Up!
+        /*When provided with a number between 0-9, return it in words. Input :: 1 - Output :: "One".
+        If your language supports it, try using a switch statement.*/
 
           function switchItUp(number){
             //Write your own Code!
@@ -1126,3 +1126,22 @@ function sumArray(array) {
                     return 'Unknown number';
             }
              } 
+
+
+        // Will there be enough space?
+        /*The Story: Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+        Task Overview: You have to write a function that accepts three parameters:
+        cap is the amount of people the bus can hold excluding the driver.
+        on is the number of people on the bus excluding the driver.
+        wait is the number of people waiting to get on to the bus excluding the driver.
+        If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+        Usage Examples: cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
+        cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+*/
+
+             function enough(cap, on, wait) {
+              // your code here
+              let totalPassengers = on + wait;
+              
+              return totalPassengers <= cap ? 0 : -1*(cap - (totalPassengers))
+            }
