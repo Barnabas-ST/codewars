0d7f96ffa7b59d0eb61fbdacee27fa04eea0cce1
Barnabas-ST <smarttraxto@gmail.com>
2022-12-11 09:30:55 -0500

    added my solution to altERnaTIng cAsE <=> ALTerNAtiNG CaSe codewars problem

--- js/main.js
+++ js/main.js
@@ -1245,3 +1245,20 @@ function expressionMatter(a, b, c) {
   
   return Math.max(allAdd, allMultiplication, bracketAddSecondThird, addFirstSecond, addSecondThird, bracketAddFirstSecond)// highest achievable result
 }
+
+
+//  altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+/*Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+For example: "hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"*/
+
+String.prototype.toAlternatingCase = function () {
+  // Define your method here :)
+  return this.split('').map(x => x == x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join("")
+}
