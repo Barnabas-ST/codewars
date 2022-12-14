70e66547d62029c64ec9b736e97cd59528935e81
Barnabas-ST <smarttraxto@gmail.com>
2022-12-14 06:34:20 -0500

    added my solution to the Is it even? codewars problem

--- js/main.js
+++ js/main.js
@@ -1311,3 +1311,16 @@ function move (position, roll) {
   
   return position !== 0 ? diceMove+position : diceMove
 }
+
+
+//  Is it even?
+/*In this Kata we are passing a number (n) into a function. Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false. Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+function testEven(n) {
+  //Your awesome code here!
+
+return n % 2 == 0 ? true : false
+}
