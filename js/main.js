48fb405e1fd0b908a90f35feb843a4fcbb10a555
Barnabas-ST <smarttraxto@gmail.com>
2022-12-29 06:41:58 -0500

    added my solution to Filling an array codewars problem

--- js/main.js
+++ js/main.js
@@ -1783,3 +1783,21 @@ function howManyLightsabersDoYouOwn(name) {
   
   return name != "Zach" ? anyoneElse : 18
 }
+
+
+//  Filling an array (part 1)
+/*We want an array, but not just any old array, an array with contents!
+Write a function that produces an array with the numbers 0 to N-1 in it.
+For example, the following code will result in an array containing the numbers 0 to 4:
+arr(5) // => [0,1,2,3,4]
+Note: The parameter is optional. So you have to give it a default value.*/
+
+const arr = N => {
+  let nArr = [];
+  
+  for(let i = 0; i < N; i++){
+    nArr.push(i)
+  }
+  
+  return nArr
+}
