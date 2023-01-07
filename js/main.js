1372c4fdf43d936dbc6805e4d7d7f7f860bff545
Barnabas-ST <smarttraxto@gmail.com>
2023-01-07 08:37:42 -0500

    added my solution to Name Shuffler code wars problem

--- js/main.js
+++ js/main.js
@@ -1993,3 +1993,15 @@ function plural(n) {
   
   return n > 1 || n < 1 ? true : false
 }
+
+
+//  Name Shuffler
+/*Write a function that returns a string in which firstname is swapped with last name.
+Example(Input --> Output)
+"john McClane" --> "McClane john"*/
+
+function nameShuffler(str){
+  //Shuffle It
+  
+  return str.split(' ').reverse().join(' ')
+}
