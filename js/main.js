67396cb0226334e0d43572f688be68069ab044ee
Barnabas-ST <smarttraxto@gmail.com>
2023-01-09 22:58:48 -0500

    added my solution to vowel remover codewars problem

--- js/main.js
+++ js/main.js
@@ -2037,3 +2037,19 @@ function warnTheSheep(queue) {
   
   return spotWolf == (queue.length - 1) ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${Math.abs((spotWolf + 1) - queue.length)}! You are about to be eaten by a wolf!`
 }
+
+
+//  Vowel remover
+/*Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata*/
+
+function shortcut (string) {
+  
+  return string.replace(/[aeiou]/g,'');
+}
