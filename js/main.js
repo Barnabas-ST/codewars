dda837ecb82959c76fd36cc2f997c08014f982f4
Barnabas-ST <smarttraxto@gmail.com>
2023-01-16 17:24:46 -0500

    added my solution to Holiday VIII - Duty Free codewars problem

--- js/main.js
+++ js/main.js
@@ -2195,3 +2195,31 @@ function pipeFix(numbers){
 
 return Array.from({length: numbers.slice(-1) - numbers[0] + 1}, (_,index) => index + numbers[0])  
 }
+
+//OR 
+
+function pipeFix(numbers){
+  var first = numbers[0];
+  var last = numbers[numbers.length-1];
+  
+  var arr = [];
+  for(var i = first; i <= last; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+
+//  Holiday VIII - Duty Free
+/*The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+All inputs will be integers. Please return an integer. Round down.
+*/
+
+function dutyFree(normPrice, discount, hol){
+
+  let discPrice = normPrice * discount/100;
+  
+  return Math.floor(hol/discPrice)
+}
