c746950fbaa7600c26471d7885e5d062a288ab1a
Barnabas-ST <smarttraxto@gmail.com>
2023-01-30 22:10:41 -0500

    added my solution to Grasshopper - Terminal game combat function codewars problem

--- js/main.js
+++ js/main.js
@@ -2392,3 +2392,15 @@ function howManyDalmatians(number){
         :dogs[2];
   
 }
+
+
+//  Grasshopper - Terminal game combat function
+/*Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. 
+Health can't be less than 0.*/
+
+function combat(health, damage) {
+  // Write your code here
+  
+  
+  return health > damage ? health - damage : 0
+}
