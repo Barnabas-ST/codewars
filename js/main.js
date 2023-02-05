1d2978b1b1e6217c4e42aac9a0a7990592769b71
Barnabas-ST <smarttraxto@gmail.com>
2023-02-05 16:46:49 -0500

    added my solution to String cleaning code wars problem

--- js/main.js
+++ js/main.js
@@ -2517,3 +2517,21 @@ function validateUsr(username) {
   //$ is an Assertion(mdn) Matches the end of input
   //^ is an Assertion(mdn) Matches the beginning of input
 }
+
+
+//  String cleaning
+/*Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. 
+At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+Examples (input -> output)
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. 
+Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.*/
+
+function stringClean(s){
+  // Function will return the cleaned string
+  
+  return `${s}`.replace(/\d/g,'')
+}
