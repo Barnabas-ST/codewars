4d7f73ea4b58ee08377bab4c91a978ae0f8a5692
Barnabas-ST <smarttraxto@gmail.com>
2023-02-11 08:27:51 -0500

    added my solution to Swap Values codewars problem

--- js/main.js
+++ js/main.js
@@ -2619,3 +2619,16 @@ function reverse(string){
   
   return string.split(' ').reverse().join(' ')
 }
+
+
+//  Swap Values
+/*I would like to be able to pass an array with two elements to my swapValues function to swap the values. 
+However it appears that the values aren't changing.
+Can you figure out what's wrong here?*/
+
+function swapValues(arr) {
+  var args = arr
+  var temp = args[0];
+  args[0] = args[1];
+  args[1] = temp;
+}
