7529dd8274b83265ffd6166151078baa8d310450
Barnabas-ST <smarttraxto@gmail.com>
2023-02-18 07:52:16 -0500

    added my solution to Sum of differences in array codewars problem

--- js/main.js
+++ js/main.js
@@ -2722,3 +2722,37 @@ var websites = [];*/
 // add the value "codewars" to the already defined websites array
 
 websites.push("codewars")
+
+
+//  Sum of differences in array
+/*Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).*/
+
+function sumOfDifferences(arr) {
+  
+  function compareNumbers(a, b) {
+  return b - a;
+}
+  let descArr = arr.sort(compareNumbers)
+  
+  let sum = 0
+  
+  if (arr.length != 0) {
+    
+    for (let i = 0; i < descArr.length - 1; ++i) {
+    sum += descArr[i] - descArr[i + 1];
+      
+    }
+    
+  } else {
+    return 0
+  }
+  
+  return sum
+  
+  }
