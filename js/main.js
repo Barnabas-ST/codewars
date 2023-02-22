578372a7b210ecad9f91bb9259d2533af1667394
Barnabas-ST <smarttraxto@gmail.com>
2023-02-21 21:31:26 -0500

    added my solution to Remove First and Last Character Part Two codewars problem

--- js/main.js
+++ js/main.js
@@ -2780,3 +2780,25 @@ Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)*
       
       return `Position of alphabet: ${letter.charCodeAt() - 96}`
     }
+
+
+    //  Remove First and Last Character Part Two
+    /*You are given a string containing a sequence of character sequences separated by commas.
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL*/
+
+    function array(arr){
+      //Good luck
+      
+        let strArr = arr.split(',').slice(1,-1)
+       
+        
+        return strArr.length < 1 ? null : strArr.join(' ')
+      }
