5c6a5b3b3e122cdef8cf4f8742483f8c7d463fd4
Barnabas-ST <smarttraxto@gmail.com>
2023-03-09 23:26:00 -0500

    added my solution to How old will I be in 2099? codewars problem

--- js/main.js
+++ js/main.js
@@ -2862,3 +2862,26 @@ function sameCase(a, b){
   return /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1
 
 }
+
+
+// How old will I be in 2099?
+/*Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+"..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+Good Luck!*/
+
+function  calculateAge(birthYear, targetYear) {
+  
+  // enter your code here.
+    
+    let age = targetYear - birthYear;
+    
+    return age > 0 && age <= 1 ? `You are ${age} year old.` 
+        : age > 1 ? `You are ${age} years old.`
+        : age >= -1 && age < 0 ? `You will be born in ${age*-1} year.`
+        : age < -1 ? `You will be born in ${age*-1} years.`
+        : age == 0 ?  "You were born this very year!"
+        : 'Got nothing'
+  
+  }
