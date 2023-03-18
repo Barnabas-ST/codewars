add7eb19ca180df168c01c9d0386b69eb2c4babd
Barnabas-ST <smarttraxto@gmail.com>
2023-03-18 09:07:17 -0400

    added my solution to Formatting decimal places #0 codewars problem

--- js/main.js
+++ js/main.js
@@ -3141,3 +3141,17 @@ function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
        return pontoonDistance/youSpeed < sharkDistance/(sharkSpeed) ? "Alive!" :"Shark Bait!"
      }
 }
+
+
+//Formatting decimal places #0
+/*Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+*/
+
+function twoDecimalPlaces(n) {
+  // Your code here
+  
+  return +n.toFixed(2)
+}
