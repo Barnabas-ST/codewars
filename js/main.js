4e6ea4433c05a9a08848f6e8a3fa23c7a9dd4e74
Barnabas-ST <smarttraxto@gmail.com>
2023-03-21 23:25:28 -0400

    added my solution to Define a card suit codewars problem

--- js/main.js
+++ js/main.js
@@ -3187,3 +3187,23 @@ var Ghost = function() {
   
   this.color = ['white','yellow','purple','red'][Math.floor(Math.random() * 4)]
 };
+
+
+//  Define a card suit
+
+/*You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+Our deck (is preloaded):
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'*/
+
+function defineSuit(card) {
+  // good luck
+  
+  if (card.includes('♣')) return 'clubs'
+    if (card.includes('♦')) return 'diamonds'
+      if (card.includes('♥')) return 'hearts'
+        if (card.includes('♠')) return 'spades'
+  
+}
