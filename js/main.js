393449b8efc545557ea555a67edf3765cc6e3a71
Barnabas-ST <smarttraxto@gmail.com>
2023-03-27 03:27:35 -0400

    added my solution to Basic subclasses - Adam and Eve codewars problem

--- js/main.js
+++ js/main.js
@@ -3287,3 +3287,42 @@ class Ship {
     return this.draft - (this.crew*1.5) > 20 ? true : false
   }
 }
+
+
+//  Basic subclasses - Adam and Eve
+/*According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
+You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). 
+The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. 
+Both objects have to be subclasses of Human. 
+Your job is to implement the Human, Man and Woman classes.
+*/
+
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+      // code
+      const Adam = new Man();
+      const Eve = new Woman();
+      return [Adam, Eve]
+    }
+  }
+  // code
+  
+  
+  class Human {
+    constructor() {
+      
+    }
+  }
+  
+  class Man extends Human {
+    cosntructor(){
+    }
+  }
+  
+  class Woman extends Human {
+    cosntructor(){
+    }
+  }
