ff5be1ff0c8ba31f18aadacbd9261c395afb517a
Barnabas-ST <smarttraxto@gmail.com>
2023-03-27 23:42:32 -0400

    added my solution to Regexp Basics - is it a digit? codewars problem

--- js/main.js
+++ js/main.js
@@ -3326,3 +3326,14 @@ class God{
     cosntructor(){
     }
   }
+
+
+  //  Regexp Basics - is it a digit?
+  /*Implement String#digit? (in Java StringUtils.isDigit(String)), 
+  which should return true if given object is a digit (0-9), false otherwise.
+
+*/
+
+  String.prototype.digit = function() {
+    return /^\d$/.test(this);
+  };
