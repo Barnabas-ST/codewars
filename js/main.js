db97c2e913bc20164ab968abc4a07a61c14eec27
Barnabas-ST <smarttraxto@gmail.com>
2023-03-28 23:27:11 -0400

    added my solution to Exclamation marks series #2: Remove all exclamation marks from the end of sentence codewars problem

--- js/main.js
+++ js/main.js
@@ -3337,3 +3337,22 @@ class God{
   String.prototype.digit = function() {
     return /^\d$/.test(this);
   };
+
+
+  /*Exclamation marks series #2: Remove all exclamation marks from the end of sentence*/
+  /*Description: Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+  function remove (string) {  
+  
+  
+    return string.replace(/!+$/, '');
+  }
