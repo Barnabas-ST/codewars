e63f38c7a5a93fecbc820b34ec02521718539b5b
Barnabas-ST <smarttraxto@gmail.com>
2023-04-01 18:05:15 -0400

    added my solution to No Loops 2 - You only need one codewars problem

--- js/main.js
+++ js/main.js
@@ -3377,3 +3377,15 @@ Return value as boolean values, true for the string to contains "English", false
 
 Object.freeze(MrFreeze) 
 // mark the MrFreeze object instance as frozen
+
+
+//  No Loops 2 - You only need one
+/**** No Loops Allowed ***
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+Array can contain numbers or strings. x can be either. 
+Return true if the array contains the value, false if not. With strings you will need to account for case.*/
+
+function check(a,x){
+  
+  return(a.includes(x))
+};
