21a5eedefc100acd7bc9213f16943deda5a3d7f7
Barnabas-ST <smarttraxto@gmail.com>
2023-04-02 10:21:58 -0400

    added my solution to Grasshopper - Combine strings codewars problem

--- js/main.js
+++ js/main.js
@@ -3389,3 +3389,18 @@ function check(a,x){
   
   return(a.includes(x))
 };
+
+
+//  Grasshopper - Combine strings
+/*Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+Example:
+combineNames('James', 'Stevens')
+returns:
+'James Stevens'*/
+
+// Create the combineNames function here
+
+function combineNames(firstName, lastName){
+  return `${firstName} ${lastName}`
+}
